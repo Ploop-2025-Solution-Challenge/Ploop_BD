6c3dee9cdb1ae2d66b58dd569ec27dab7a55a8d7
Yoolim Han <ninetale22@gmail.com>
2025-04-16 15:29:33 +0900

    fix: test 코드 주석처리

--- src/test/java/com/example/ploop_backend/PloopBackendApplicationTests.java
+++ src/test/java/com/example/ploop_backend/PloopBackendApplicationTests.java
@@ -1,13 +1,14 @@
-package com.example.ploop_backend;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class PloopBackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.ploop_backend;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class PloopBackendApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
+///
