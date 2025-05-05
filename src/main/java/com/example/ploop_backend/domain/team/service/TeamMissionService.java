054f298d8a1b45079179a154a886dd239130d3a1
xxrainow <sjw1770@seoultech.ac.kr>
2025-05-05 23:59:29 +0900

    team db 직접 조회

--- src/main/java/com/example/ploop_backend/domain/team/service/TeamMissionService.java
+++ src/main/java/com/example/ploop_backend/domain/team/service/TeamMissionService.java
@@ -24,7 +24,7 @@ public class TeamMissionService {
     private final UserMissionRepository userMissionRepository;
 
     @Transactional
-    public int assignRandomMissionsToTeam(Team team) {
+    public void assignRandomMissionsToTeam(Team team) {
         User user1 = team.getUser1();
         User user2 = team.getUser2();
         List<Mission> missions = missionRepository.findRandomThree();
@@ -40,7 +40,5 @@ public class TeamMissionService {
             userMissionRepository.save(UserMission.builder().user(user1).teamMission(teamMission).build());
             userMissionRepository.save(UserMission.builder().user(user2).teamMission(teamMission).build());
         }
-
-        return missions.size();
     }
 }
