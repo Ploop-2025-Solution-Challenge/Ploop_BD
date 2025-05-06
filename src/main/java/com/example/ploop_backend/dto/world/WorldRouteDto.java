d2bc46b90969f8649f06d7fa4c55814adca1afc2
xxrainow <sjw1770@seoultech.ac.kr>
2025-05-06 15:07:29 +0900

    constructor

--- src/main/java/com/example/ploop_backend/dto/world/WorldRouteDto.java
+++ src/main/java/com/example/ploop_backend/dto/world/WorldRouteDto.java
@@ -1,12 +1,16 @@
 package com.example.ploop_backend.dto.world;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class WorldRouteDto {
     private Long routeId;
     private List<List<Double>> activityRoute; // JSON 문자열을 디코딩해서 사용.
