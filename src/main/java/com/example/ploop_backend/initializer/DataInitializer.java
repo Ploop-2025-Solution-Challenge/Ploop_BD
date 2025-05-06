5c4d0b176db00f99e0ef855830d69b72971ae692
xxrainow <sjw1770@seoultech.ac.kr>
2025-05-06 21:03:28 +0900

    값 수정

--- src/main/java/com/example/ploop_backend/initializer/DataInitializer.java
+++ src/main/java/com/example/ploop_backend/initializer/DataInitializer.java
@@ -19,11 +19,11 @@ public class DataInitializer implements CommandLineRunner {
     public void run(String... args) {
         if (missionRepository.count() == 0) {
             missionRepository.saveAll(List.of(
-                    Mission.builder().name("Picking up 1 empty can").description("길가나 공원에 떨어진 빈 캔을 수거하세요.").category(Category.EMPTY_CAN).build(),
-                    Mission.builder().name("Collecting 3 plastic bottles").description("플라스틱 병 3개를 주워 정해진 장소에 버려주세요.").category(Category.PLASTIC_BOTTLE).build(),
-                    Mission.builder().name("Gathering 5 plastic bottle caps").description("페트병 뚜껑을 5개 수거해보세요.").category(Category.BOTTLE_CAP).build(),
-                    Mission.builder().name("Collecting 3 paper cups").description("쓰레기로 버려진 종이컵을 3개 주워주세요.").category(Category.PAPER_CUP).build(),
-                    Mission.builder().name("Removing 2 plastic bags").description("날아다니는 비닐봉지 또는 버려진 비닐 2개를 주워주세요.").category(Category.VINYL_BAG).build()
+                    Mission.builder().name("Pick up 1 empty can").description("길가나 공원에 떨어진 빈 캔을 수거하세요.").category(Category.EMPTY_CAN).build(),
+                    Mission.builder().name("Collect 3 plastic bottles").description("플라스틱 병 3개를 주워 정해진 장소에 버려주세요.").category(Category.PLASTIC_BOTTLE).build(),
+                    Mission.builder().name("Gather 5 plastic bottle caps").description("페트병 뚜껑을 5개 수거해보세요.").category(Category.BOTTLE_CAP).build(),
+                    Mission.builder().name("Collect 3 paper cups").description("쓰레기로 버려진 종이컵을 3개 주워주세요.").category(Category.PAPER_CUP).build(),
+                    Mission.builder().name("Remove 2 plastic bags").description("날아다니는 비닐봉지 또는 버려진 비닐 2개를 주워주세요.").category(Category.VINYL_BAG).build()
             ));
 
         }
